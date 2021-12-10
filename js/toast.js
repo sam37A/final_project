751b30fc5b01f825b99fc9aa09225ba3b90f4cd4
sam37A <sahilsan30@gmail.com>
2021-12-09 20:50:18 -0800

    add media query

--- js/toast.js
+++ js/toast.js
@@ -1,9 +1,11 @@
-var toastTrigger = document.getElementById('liveToastBtn')
-var toastLiveExample = document.getElementById('liveToast')
-if (toastTrigger) {
-  toastTrigger.addEventListener('click', function () {
-    var toast = new bootstrap.Toast(toastLiveExample)
+(function () {
+  var toastTrigger = document.getElementById('liveToastBtn')
+  var toastLiveExample = document.getElementById('liveToast')
+  if (toastTrigger) {
+      toastTrigger.addEventListener('click', function () {
+          var toast = new bootstrap.Toast(toastLiveExample)
 
-    toast.show()
-  })
-}
+          toast.show()
+      })
+  }
+})();
--- js/tooltips.js
+++ js/tooltips.js
@@ -1,8 +1,12 @@
 
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+(function () {
+  var toastTrigger = document.getElementById('liveToastBtn')
+  var toastLiveExample = document.getElementById('liveToast')
+  if (toastTrigger) {
+      toastTrigger.addEventListener('click', function () {
+          var toast = new bootstrap.Toast(toastLiveExample)
 
-
-
+          toast.show()
+      })
+  }
+})();
